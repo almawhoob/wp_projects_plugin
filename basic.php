07b5acadbba825678143af6e927e2e96f0c6fb39
almawhoob <mawhoob@gmail.com>
2017-07-21 22:33:27 -0400

    Project info widget

--- basic.php
+++ basic.php
@@ -103,7 +103,10 @@ function wppp_project_custom_post_type() {
                                'singular_name' => __('Projects'),
                            ],
                            'public'      => true,
+                        //    'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail','page-attributes' ),
+                           'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail' ),
                            'has_archive' => true,
+                           'exclude_from_search' => false,
                            'rewrite'     => ['slug' => 'projects'], // my custom slug
                             
                        ]
@@ -116,7 +119,7 @@ add_action('init', 'wppp_project_custom_post_type');
 /* Creates Taxonomy for Project Categories */
 function wporg_register_taxonomy_project_category(){
     $labels = [
-        'name'      => _x('Project Categories', 'taxonomy general name'),
+        'name'              => _x('Project Categories', 'taxonomy general name'),
         'singular_name'     => _x('Project Categories', 'taxonomy singular name'),
         'search_items'      => __('Search Project Categories'),
         'all_items'         => __('All Categories'),
@@ -168,7 +171,10 @@ function wppp_project_info_display_callback( $post ) {
     // Display code/markup goes here. Don't forget to include nonces!
      
     $values = get_post_custom( $post->ID );
-    $text = isset( $values['project_startdate'] ) ? esc_attr( $values['project_startdate'][0] ) : ”;
+    $p_startdate = isset( $values['project_startdate'] ) ? esc_attr( $values['project_startdate'][0] ) : ”;
+    $p_enddate = isset( $values['project_enddate'] ) ? esc_attr( $values['project_enddate'][0] ) : ”;
+    $p_max_members = isset( $values['project_max_members'] ) ? esc_attr( $values['project_max_members'][0] ) : ”;
+
     // $selected = isset( $values['my_meta_box_select'] ) ? esc_attr( $values['my_meta_box_select'][0] ) : ”;
     // $check = isset( $values['my_meta_box_check'] ) ? esc_attr( $values['my_meta_box_check'][0] ) : ”;
     
@@ -176,14 +182,18 @@ function wppp_project_info_display_callback( $post ) {
     wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
     ?>
 
-        <p>
+         <p>
             <label for="project_startdate">Project Start Date</label>
-            <input type="date" name="project_startdate" id="project_startdate" value="<?php echo $text; ?> "/>
+            <input type="date" name="project_startdate" id="project_startdate" placeholder="YYYY-MM-DD" value="<?php echo $p_startdate; ?>"/>
         </p>
         <p>
             <label for="project_enddate">Project End Date</label>
-            <input type="date" name="project_enddate" id="project_enddate" />
+            <input type="date" name="project_enddate" id="project_enddate" placeholder="YYYY-MM-DD" value="<?php echo $p_enddate; ?>"/>
         </p>
+        <p>
+            <label for="project_max_members">Max Number of Participants</label>
+            <input type="number" min="0" name="project_max_members" id="project_max_members" value="<?php echo $p_max_members; ?>"/>
+        </p> 
     <?php    
 }
 
@@ -208,6 +218,12 @@ function cd_meta_box_save( $post_id ) {
     // Make sure your data is set before trying to save it
     if( isset( $_POST['project_startdate'] ) )
         update_post_meta( $post_id, 'project_startdate', wp_kses( $_POST['project_startdate'], $allowed ) );
+
+    if( isset( $_POST['project_enddate'] ) )
+        update_post_meta( $post_id, 'project_enddate', wp_kses( $_POST['project_enddate'], $allowed ) );
+
+    if( isset( $_POST['project_max_members'] ) )
+        update_post_meta( $post_id, 'project_max_members', wp_kses( $_POST['project_max_members'], $allowed ) );        
          
     // if( isset( $_POST['my_meta_box_select'] ) )
     //     update_post_meta( $post_id, 'my_meta_box_select', esc_attr( $_POST['my_meta_box_select'] ) );
@@ -227,3 +243,97 @@ function wppp_save_meta_box( $post_id ) {
     // Save logic goes here. Don't forget to include nonce checks!
 }
 add_action( 'save_post', 'wppp_save_meta_box' );
+
+
+
+
+
+
+
+
+
+    // Register and load the widget
+    function wpb_load_widget() {
+        register_widget( 'wpb_widget' );
+    }
+    add_action( 'widgets_init', 'wpb_load_widget' );
+
+    // Creating the widget 
+    class wpb_widget extends WP_Widget {
+
+    function __construct() {
+        parent::__construct(
+
+        // Base ID of your widget
+        'wpb_widget', 
+
+        // Widget name will appear in UI
+        __('WPPP Project Info Widget', 'wpb_widget_domain'), 
+
+        // Widget description
+        array( 'description' => __( 'WPPP Project Info widget displays project information on theme.', 'wpb_widget_domain' ), ) 
+        );
+    }
+
+    // Creating widget front-end
+    public function widget( $args, $instance ) {
+        if ( is_single() ) {
+            // making sure that the post type is "wppp_project"
+            if ('wppp_project' === get_post_type()) {
+            $title = apply_filters( 'widget_title', $instance['title'] );
+
+            // before and after widget arguments are defined by themes
+            echo $args['before_widget'];
+            if ( ! empty( $title ) )
+                echo $args['before_title'] . $title . $args['after_title'];
+
+            // This is where you run the code and display the output
+            // echo __( 'Hello, World!', 'wpb_widget_domain' );
+            
+            // get post id
+            $queried_object = get_queried_object();
+            if ( $queried_object ) {
+                $post_id = $queried_object->ID;
+                // echo $post_id;
+            }
+
+            if ('wppp_project' === get_post_type()) {
+                echo '<p>Project Starts: <br/> <strong>'; 
+                echo get_post_meta($post_id, 'project_startdate', true);
+                echo '</strong><br/><br/> Project Ends: <br/> <strong>'; 
+                echo get_post_meta($post_id, 'project_enddate', true); 
+                echo '</strong><br/></p> <p>';
+                echo 'Max number of participants: <br/> <strong>';
+                echo get_post_meta($post_id, 'project_max_members', true);
+                echo '</strong></p>'; 
+            };
+            echo $args['after_widget'];
+            }//end of if-statement for wppp_project
+        }
+    }
+
+
+    // Widget Backend 
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        } else {
+            $title = __( 'Project Information Widget', 'wpb_widget_domain' );
+        }
+
+    // Widget admin form
+        ?>
+            <p>
+                <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+                <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            </p>
+        <?php 
+    }
+        
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
+} // Class wpb_widget ends here
